afab907867f82522b852a98903b80dcb1ba71861
Humberto Escobedo <hescobedo@hotmail.com>
2023-02-11 23:26:26 -0600

    Initial commit

---
+++ src/main/java/com/okta/developer/jugtours/JugtoursApplication.java
@@ -0,0 +1,15 @@
+package com.okta.developer.jugtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+public class JugtoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JugtoursApplication.class, args);
+    }
+
+}
